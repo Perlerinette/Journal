3ef5d38c42dfa383e237a23ca54ce2be5a700716
Perlerinette <laurine.reyre@gmail.com>
2021-01-22 12:02:06 -0500

    GET journal - display All

--- client/get-scripts.js
+++ client/get-scripts.js
@@ -86,7 +86,71 @@ function displayMine() {
     *** DISPLAY ALL ***
 ************************** */
 function displayAll() {
-    console.log('displayAll Function Called')
+    console.log('displayAll Function Called');
+
+    const accessToken = localStorage.getItem('SessionToken');
+    fetch('http://localhost:3000/journal/', {
+        method: 'GET',
+        headers: new Headers({
+            'Content-Type': 'application/json',
+            'Authorization': accessToken
+        })
+    })
+    .then( function(response){ return response.json() })
+    .catch( function(err) { console.error('Error:', error)})
+
+    .then(function (response) {
+        console.log(response);
+
+        let display = document.getElementById('journals')
+        for(i = 0; i = display.childNodes.length; i++) {
+            display.removeChild(display.firstChild);
+        }
+
+        if(response.length === 0) {
+            let display = document.getElementById('journals');
+            let header = document.createElement('h5');
+
+            display.appendChild(header);
+            header.textContent = "There are no posts yet!"
+            header.setAttribute("class", "noPosts");
+
+        } else{
+            for (i=0; i < response.length; i++) {
+                let display = document.getElementById('journals');
+                let card = document.createElement('div');
+                let body = document.createElement('div');
+                let header = document.createElement('h5');
+                let subtitle = document.createElement('h6');
+                let para = document.createElement('p');
+
+                let current = response[i];
+                let title = current.title;
+                let date = current.date;
+                let entry = current.entry;
+
+                display.appendChild(card);
+                card.appendChild(body);
+                body.appendChild(header);
+                body.appendChild(subtitle);
+                body.appendChild(para);
+
+                card.setAttribute('id', current.id);
+                card.setAttribute('class', 'card');
+                body.setAttribute('class', 'card-body');
+                header.setAttribute('class', 'card-title');
+                subtitle.setAttribute('class', 'card-subtitle mb-2 text-muted');
+                para.setAttribute('class', 'card-text');
+
+                header.textContent = title;
+                subtitle.textContent = date;
+                para.textContent = entry;
+
+            }
+        }
+    })
+
+
 }
 
 
